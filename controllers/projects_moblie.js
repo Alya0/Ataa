f82a1a73fc0aad80a69735334a749ce4424f28a2
Alya Msouti <alyaalmsouti@gmail.com>
2022-07-18 16:13:15 +0300

    mobile_projects done + fixed not found error

--- controllers/projects_moblie.js
+++ controllers/projects_moblie.js
@@ -1,10 +1,27 @@
+const {Project} = require('../models')
+const { Op } = require("sequelize");
+const {StatusCodes} = require('http-status-codes')
+const {NotFoundError} = require('../errors')
 
 const getAll = async(req, res)=>{
-	res.send('all');
+	const projects = await Project.findAll({
+		where:{
+			[Op.or]:[
+				{project_status : 'ongoing'},
+				{project_status : 'مستمر'}
+			]
+		}
+	})
+	res.status(StatusCodes.OK).json(projects);
 }
 
 const getOne = async(req, res)=>{
-	res.send('one');
+	const {id} = req.params
+	const project = await Project.findByPk(id)
+	if( !project ){
+		throw new NotFoundError(`No project with id ${id}`)
+	}
+	res.status(StatusCodes.OK).json(project)
 }
 module.exports = {
 	getAll, 
